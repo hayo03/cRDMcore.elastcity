a051ee7a2654387956ec473b6bdf489f8e3463f3
hayo03 <37178100+hayo03@users.noreply.github.com>
2019-03-04 15:29:34 +0100

    Create AtomicresourceImpl.java

---
+++ cloudelasticity.crmd/src/cloudelasticity/crmd/impl/AtomicresourceImpl.java
@@ -0,0 +1,37 @@
+/**
+ */
+package cloudelasticity.crmd.impl;
+
+import cloudelasticity.crmd.Atomicresource;
+import cloudelasticity.crmd.crmdPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Atomicresource</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class AtomicresourceImpl extends CloudresourceImpl implements Atomicresource {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected AtomicresourceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return crmdPackage.Literals.ATOMICRESOURCE;
+	}
+
+} //AtomicresourceImpl
