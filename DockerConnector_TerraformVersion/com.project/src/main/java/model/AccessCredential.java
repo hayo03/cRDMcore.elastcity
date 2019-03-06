20560cd92110c673921382c6f69d78aff05fbebd
hayo03 <37178100+hayo03@users.noreply.github.com>
2019-03-06 22:36:13 +0100

    Create AccessCredential.java

---
+++ DockerConnector_TerraformVersion/com.project/src/main/java/model/AccessCredential.java
@@ -0,0 +1,42 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AccessCredential{
+	
+
+	private String credentialname;
+	@JsonProperty("access-key")
+	private String access_key;
+	
+	private String secret_key;
+	
+	//Getters & Setters
+	public String getCredentialname() {
+		return credentialname;
+	}
+	public void setCredentialname(String credentialname) {
+		this.credentialname = credentialname;
+	}
+	public String getAccess_key() {
+		return access_key;
+	}
+	public void setAccess_key(String access_key) {
+		this.access_key = access_key;
+	}
+	public String getSecret_key() {
+		return secret_key;
+	}
+	public void setSecret_key(String secret_key) {
+		this.secret_key = secret_key;
+	}
+	
+	//Constructor
+	public AccessCredential() {
+		super();
+	}
+	
+	
+	
+
+}
